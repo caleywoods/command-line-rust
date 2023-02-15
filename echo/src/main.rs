e7eb56d2843d1ffcdb389dd7d8506fd6b12b98fd
Caley Woods <caley.woods@gsa.gov>
2023-02-15 11:02:05 -0600

    Add argument parsing using clap

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,3 +1,32 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("echo")
+                   .version("0.1")
+                   .author("Caley Woods")
+                   .about("Rust echo")
+                   .arg(
+                     Arg::with_name("text")
+                         .value_name("TEXT")
+                         .help("Input text")
+                         .required(true)
+                         .min_values(1),
+                   )
+                   .arg(
+                     Arg::with_name("omit_newline")
+                         .short("n")
+                         .help("Do not print newline")
+                         .takes_value(false),
+                   )
+                   .get_matches();
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    
+    let mut ending = "\n";
+    if omit_newline {
+        ending = "";
+    }
+
+    print!("{}{}", text.join(" "), ending);
 }
